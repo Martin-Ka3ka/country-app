14a81081d291edf8256057fac2afc35eccf8c2b0
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-06-04 09:07:57 +0200

    changes in file part II

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-//import DevTools from './DevTools';
+import DevTools from './DevTools';
 import { getCountries } from './actions/actions-countries'
 import routes from './routes';
 import './country.css'
 
 render(
     <Provider store={store}>
-        <Router history={hashHistory} routes={routes} />
+        <div>
+            <Router history={hashHistory} routes={routes} />
+            <DevTools />
+        </div>
     </Provider>,
+
     document.getElementById('root')
 );
 
